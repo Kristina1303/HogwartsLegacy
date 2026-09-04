113df95827615d09b758ec7e4d697da2b3e2f595
Krisi <82513090+Kristina1303@users.noreply.github.com>
2026-09-04 12:24:41 +0300

    Add files via upload

---
+++ HealthPotion.java
@@ -0,0 +1,25 @@
+package a12540834;
+
+public class HealthPotion extends Potion {
+	
+	private int health;
+
+    public HealthPotion(String name, int usages, int price, int weight, int health) {
+        super(name, usages, price, weight);
+        if (health < 0) throw new IllegalArgumentException();
+        this.health = health;
+    }
+
+    @Override
+    public String additionalOutputString() {
+        return "; +" + health + " HP";
+    }
+
+    @Override
+    public void useOn(MagicEffectRealization target) {
+        if (target == null) throw new IllegalArgumentException();
+        if (tryUsage()) {
+            target.heal(health);
+        }
+    }
+}
