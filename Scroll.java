1b71f88504b399a6401ab4b70eb2e2edcd2618bb
Krisi <82513090+Kristina1303@users.noreply.github.com>
2026-09-04 12:27:52 +0300

    Add files via upload

---
+++ Scroll.java
@@ -0,0 +1,22 @@
+package a12540834;
+
+public class Scroll extends MagicItem{
+	private Spell spell;
+	
+	public Scroll (String name, int usages, int price, int weight, Spell spell) {
+		super(name, usages, price, weight);
+		if(spell == null) throw new IllegalArgumentException();
+		this.spell = spell;
+	}
+	@Override
+	public String additionalOutputString () {
+		 return "; casts " + spell;
+	}
+	@Override
+    public void useOn(MagicEffectRealization target) {
+        if (tryUsage()) {
+            spell.cast(this, target); 
+        }
+	}
+
+}
