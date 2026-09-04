1823b25df13b127362b2fd8811cb09eb56a2e4d0
Krisi <82513090+Kristina1303@users.noreply.github.com>
2026-09-04 12:26:14 +0300

    Add files via upload

---
+++ ManaPotion.java
@@ -0,0 +1,26 @@
+package a12540834;
+
+public class ManaPotion extends Potion{
+
+	 private int mana;
+
+	    public ManaPotion(String name, int usages, int price, int weight, int mana) {
+	        super(name, usages, price, weight);
+	        if (mana < 0) throw new IllegalArgumentException();
+	        this.mana = mana;
+	    }
+
+	    @Override
+	    public String additionalOutputString() {
+	        return "; +" + mana + " MP";
+	    }
+
+	    @Override
+	    public void useOn(MagicEffectRealization target) {
+	        if (target == null) throw new IllegalArgumentException();
+	        if (tryUsage()) {
+	            target.enforceMagic(mana);
+	        }
+	    }
+	}
+
